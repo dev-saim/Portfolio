a623bea6cb08d8222f80760cffcfef26d0903142
Saim Asif <saimasif885@gmail.com>
2025-05-21 21:15:59 +0200

    last change

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
+import dynamic from "next/dynamic";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+// 👇 Dynamic import of potentially browser-dependent components
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const Blog = dynamic(() => import("./components/homepage/blog"), { ssr: false });
 
 async function getData() {
   const res = await fetch(
@@ -37,6 +40,7 @@ export default async function Home() {
       <Experience />
       <Skills />
       <Projects />
+      {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </>
   );
